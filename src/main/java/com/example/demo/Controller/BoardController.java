55abec95a0de4ef7b23d56dd646b88a9bc147ff7
arum <arumshin.dev@gmail.com>
2024-06-01 11:05:43 +0900

    controller

--- src/main/java/com/example/demo/Controller/BoardController.java
+++ src/main/java/com/example/demo/Controller/BoardController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//전통적인 Controller로 View를 반환하기 위해 사용한다고 한다.
 @RequestMapping("/board/")
 public class BoardController {
     @GetMapping("/hello")
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController//controller에 @ResponseBody가 추가된것, JSON 객체 형태로 데이터를 반환할 때 사용한다고 한다.
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "helloWorld";
     }
 
 }
